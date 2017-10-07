9c1f7d578ce72454cad089fd483d50f27f88f3e2
bedunkevich <stas.bedunkevich@gmail.com>
2017-10-07 19:20:56 +0300

    smooth scroll

--- src/components/Persons.js
+++ src/components/Persons.js
@@ -34,25 +34,44 @@ class Persons extends React.Component {
     });
   }*/
 
-  static  propTypes = {
+  static propTypes = {
     onClick: PropTypes.func.isRequired,
     getProfile: PropTypes.func.isRequired,
     scene: PropTypes.object.isRequired,
     selectedPerson: PropTypes.object
   };
 
+  constructor(props) {
+    super(props);
+    this.state = { persons: 0 };
+  }
+
+  componentWillReceiveProps(props) {
+    const { scene } = props;
+    const persons = scene.persons;
+    const size = _.size(persons);
+
+    if (this.state.persons != size) {
+      let scroll_left = (_.size(persons) - 1) * (PERSON_WIDTH + 10);
+      this.setState({ persons: size });
+      this.refs.scroll_content.scrollLeft = scroll_left;
+    }
+
+
+  }
+
   render() {
     const { scene, onClick, selectedPerson, getProfile } = this.props;
     const persons = scene.persons;
-    const div_width = _.size(persons) * (PERSON_WIDTH + 10);
+    const div_width = (_.size(persons) + 1) * (PERSON_WIDTH + 10);
 
     return (
       <div style={{margin: '0px', padding: '0px', right: '0px', left: '0px',
                   background: '', overflow: 'hidden', position: 'absolute', top: this.props.isElectron ? '70px' : '50px', bottom: '0px', justifyContent: 'center',
                   display: 'flex'
                 }}>
-        <div style={{overflowX: 'auto', overflowScrolling: "touch", height: '100%',
-        WebkitOverflowScrolling: "touch", overflowY: 'hidden', margin: '0px',
+        <div ref='scroll_content' style={{overflowX: 'auto', overflowScrolling: "touch", height: '100%',
+        WebkitOverflowScrolling: "touch", overflowY: 'hidden', margin: '0px', scrollBehavior: 'smooth',
         flexShrink: 1, padding: '0px'}}>
           <ul style={{height: '98%', width: div_width + 5 + 'px', display: 'flex', marginLeft: '10px'}}>
             { _.map(persons, person =>
@@ -64,6 +83,9 @@ class Persons extends React.Component {
                 {...person}
               />
             )}
+            <li style={{width: PERSON_WIDTH + 'px', height: '100%', border: '1px solid #444', marginRight: '10px'}}>
+              &nbsp;
+            </li>
           </ul>
         </div>
       </div>
